400e62830f0f606cfbdba3a05a1f8c0d0e93b8d7
Phil G <philipposgeorgiou7@gmail.com>
2023-09-19 20:59:01 +0000

    content +  style updates
    changed dynamic styling applied to <NavLink>'s; added margin to Shop.jsx; changed button and item card colors (from yellow to amber) in ContactForm.jsx, Display.jsx, Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,56 +1,76 @@
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-    return (
-        // should these be LIs?
-        <header>
-            <nav>
-                <ul className="flex flex-row place-content-center gap-4 md:gap-10 lg:gap-20">
-                    <li>
-                        <NavLink
-                            to="/"
-                            className="rounded-md  hover:bg-yellow-500 hover:text-black visited:bg-yellow-500" // visited property doesn't display as intended
-                            end
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/about-us"
-                            className="rounded-md  hover:bg-yellow-500 hover:text-black "
-                        >
-                            About Us
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/contact"
-                            className="rounded-md  hover:bg-yellow-500 hover:text-black "
-                        >
-                            Contact
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/comps"
-                            className="rounded-md  hover:bg-yellow-500 hover:text-black "
-                        >
-                            Comps
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/shop"
-                            className="rounded-md  hover:bg-yellow-500 hover:text-black "
-                        >
-                            Shop
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    // should these be LIs?
+    <header>
+      <nav>
+        <ul className="flex flex-row place-content-center gap-4 md:gap-10 lg:gap-20">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "rounded-md  hover:bg-amber-500 hover:text-black "
+                  : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about-us"
+              className={({ isActive }) =>
+                isActive
+                  ? "rounded-md hover:bg-amber-500 hover:text-black "
+                  : undefined
+              }
+            >
+              About Us
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive
+                  ? "rounded-md hover:bg-amber-500 hover:text-black "
+                  : undefined
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/comps"
+              className={({ isActive }) =>
+                isActive
+                  ? "rounded-md hover:bg-amber-500 hover:text-black "
+                  : undefined
+              }
+            >
+              Comps
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/shop"
+              className={({ isActive }) =>
+                isActive
+                  ? "rounded-md hover:bg-amber-500 hover:text-black "
+                  : undefined
+              }
+            >
+              Shop
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navigation;
