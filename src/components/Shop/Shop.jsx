495d9c4898acfb813165ca7b550f83d4381d355c
Phil Georgiou <philipposgeorgiou7@gmail.com>
2024-01-11 18:25:17 +0000

    replace realtime database instance

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,59 +3,63 @@ import ShopItem from "./ShopItem";
 import ShopCartButton from "./ShopCartButton";
 
 const Shop = ({ onShowCart }) => {
-  const [shopItems, setShopItems] = useState([]);
+    const [shopItems, setShopItems] = useState([]);
 
-  const fetchItemsHandler = useCallback(async () => {
-    try {
-      const response = await fetch(
-        "https://doghouse-store-default-rtdb.europe-west1.firebasedatabase.app/items.json"
-      );
+    const fetchItemsHandler = useCallback(async () => {
+        try {
+            const response = await fetch(
+                "https://doghouseshop-44293-default-rtdb.europe-west1.firebasedatabase.app/items.json"
+            );
 
-      if (!response.ok) {
-        throw new Error("Cannot fetch shop items. Please try again.");
-      }
+            if (!response.ok) {
+                throw new Error("Cannot fetch shop items. Please try again.");
+            }
 
-      const data = await response.json();
+            const data = await response.json();
 
-      const loadedItems = [];
+            console.log(data);
 
-      for (const key in data) {
-        loadedItems.push({
-          id: key,
-          name: data[key].name,
-          description: data[key].description,
-          price: data[key].price,
-        });
-      }
+            const loadedItems = [];
 
-      setShopItems(loadedItems);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+            for (const key in data) {
+                loadedItems.push({
+                    id: key,
+                    name: data[key].name,
+                    description: data[key].description,
+                    price: data[key].price,
+                });
+            }
 
-  useEffect(() => {
-    fetchItemsHandler();
-  }, [fetchItemsHandler]);
+            console.log(loadedItems);
 
-  return (
-    <section className="flex flex-col place-items-center m-2">
-      <div className="mt-10">
-        <ShopCartButton showCart={onShowCart} />
-      </div>
-      <div>
-        {shopItems.map((item) => (
-          <ShopItem
-            key={item.name}
-            id={item.id}
-            description={item.description}
-            name={item.name}
-            price={item.price}
-          />
-        ))}
-      </div>
-    </section>
-  );
+            setShopItems(loadedItems);
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchItemsHandler();
+    }, [fetchItemsHandler]);
+
+    return (
+        <section className="flex flex-col place-items-center m-2">
+            <div className="mt-10">
+                <ShopCartButton showCart={onShowCart} />
+            </div>
+            <div>
+                {shopItems.map((item) => (
+                    <ShopItem
+                        key={item.name}
+                        id={item.id}
+                        description={item.description}
+                        name={item.name}
+                        price={item.price}
+                    />
+                ))}
+            </div>
+        </section>
+    );
 };
 
 export default Shop;
